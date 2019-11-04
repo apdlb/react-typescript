683cf43daa88e7f8387936d4000ad1287d313ff7
Alejandro Paez <alepdlb@gmail.com>
2019-11-04 18:28:49 +0100

    Improvements

--- src/redux/actions/AuthActions.ts
+++ src/redux/actions/AuthActions.ts
@@ -7,7 +7,10 @@ import CONSTANTS from '../../utils/constants';
 export const cleanAuth = createAction(CONSTANTS.ACTION_CLEAN_AUTH, () => {});
 export const login = createAction(CONSTANTS.ACTION_LOGIN, (body: object) => {
   return apiFetch({ method: CONSTANTS.POST, url: urlAuthLogin, body }).then(r => {
-    localStorage.setItem('jwtToken', r.access_token);
+    if (r && r.access_token) {
+      localStorage.setItem('jwtToken', r.access_token);
+    }
+
     return r;
   });
 });
