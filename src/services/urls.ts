e0de79c3dff669a2c9d788dd7161352bedf3b76a
Alejandro Paez <alepdlb@gmail.com>
2019-11-05 13:33:40 +0100

    Add url auth

--- src/services/urls.ts
+++ src/services/urls.ts
@@ -1,4 +1,5 @@
 const urlBase = `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/api/${process.env.REACT_APP_API_VERSION}/`;
 
+export const urlAuth = `${urlBase}auth`;
 export const urlAuthLogin = `${urlBase}auth/login`;
 export const urlEntities = `${urlBase}entities`;
