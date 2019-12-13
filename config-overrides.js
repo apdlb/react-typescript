292c1ed7c440d892bfb4aea7c6b57514adcd2eb0
Alejandro Paez <alepdlb@gmail.com>
2019-12-13 11:50:22 +0100

    Remove .babelrc

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,16 @@
-const {
-  override,
-  fixBabelImports,
-  addLessLoader,
-  useBabelRc
-} = require("customize-cra");
-const path = require("path");
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const path = require('path');
 
 module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
     style: true
   }),
   addLessLoader({
     modifyVars: {
-      hack: `true; @import "${path.join(__dirname, "/src/styles/index.less")}";`
+      hack: `true; @import "${path.join(__dirname, '/src/styles/index.less')}";`
     },
     javascriptEnabled: true
-  }),
-  useBabelRc()
+  })
 );
