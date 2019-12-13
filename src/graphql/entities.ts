1e4593d8d326a9382fbd5fea986a89b24c2c4493
Alejandro Paez <alepdlb@gmail.com>
2019-12-13 13:03:26 +0100

    Minor fix

--- src/graphql/entities.ts
+++ src/graphql/entities.ts
@@ -2,18 +2,18 @@ import gql from 'graphql-tag';
 
 export const initialEntities = {
   entitiesPaginated: {
-    __typename: "EntitiesPaginated",
+    __typename: 'EntitiesPaginated',
     docs: [],
     page: null,
     limit: null,
     totalDocs: null
   } as any,
   paginateEntitiesParams: {
-    __typename: "PaginateEntitiesParams",
+    __typename: 'PaginateEntitiesParams',
     page: 1,
     pageSize: 2,
-    sort: "_id",
-    order: "asc",
+    sort: '_id',
+    order: 'asc',
     field1: null,
     field2: null
   } as any
@@ -26,7 +26,7 @@ export const GET_ENTITIES_PAGINATED = gql`
 `;
 
 export const ENTITIES_PAGINATED = gql`
-  query entitiesPaginated {
+  query EntitiesPaginated {
     entitiesPaginated @client {
       docs
       page
@@ -37,13 +37,13 @@ export const ENTITIES_PAGINATED = gql`
 `;
 
 export const SET_ENTITIES_PAGINATED = gql`
-  mutation setEntitiesPaginated($entitiesPaginated: JSON!) {
+  mutation SetEntitiesPaginated($entitiesPaginated: JSON!) {
     setEntitiesPaginated(entitiesPaginated: $entitiesPaginated) @client
   }
 `;
 
 export const PAGINATE_ENTITIES_PARAMS = gql`
-  query paginateEntitiesParams {
+  query PaginateEntitiesParams {
     paginateEntitiesParams @client {
       page
       pageSize
